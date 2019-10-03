34569895828e08d5f9a9ba9c4d49000ff3987d67
r-veiga <roberto.aprende.mucho@gmail.com>
2019-10-04 00:26:44 +0200

    2019.10.04 primer commit

---
+++ src/main/java/guru/springframework/course/spr5petclinic/Spr5PetclinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.course.spr5petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spr5PetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spr5PetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/course/spr5petclinic/Spr5PetclinicApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.course.spr5petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spr5PetclinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
